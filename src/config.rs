0df28667fac4b4b4cb65368c032170aca1bb14b9
jzbor <jzbor@posteo.net>
2025-02-10 17:22:35 +0100

    Adding unimplemented options

--- src/config.rs
+++ src/config.rs
@@ -2,6 +2,14 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Config {
+    /// Delete only generations older than <OLDER> days
+    #[clap(short, long, default_value = "30")]
+    pub older: Option<usize>,
+
+    /// Keep at least <KEEP> generations
+    #[clap(short, long, default_value = "10")]
+    pub keep: Option<usize>,
+
     /// Only list generations with their age don't remove them
     #[clap(long)]
     pub list: bool,
