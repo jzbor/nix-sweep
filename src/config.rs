88cb147f414c8faac835e57246ebdf00adc81dfe
jzbor <jzbor@posteo.net>
2025-02-11 12:05:03 +0100

    Improving cli, adding --interactive and --rm

--- src/config.rs
+++ src/config.rs
@@ -2,6 +2,22 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Config {
+    /// Only list generations with their age, don't remove them
+    #[clap(long)]
+    pub list: bool,
+
+    /// Remove generations
+    #[clap(long)]
+    pub rm: bool,
+
+    /// Run nix garbage collection afterwards
+    #[clap(long)]
+    pub gc: bool,
+
+    /// Ask for confirmation before starting removal
+    #[clap(short, long)]
+    pub interactive: bool,
+
     /// Delete only generations older than <OLDER> days
     #[clap(short, long, default_value = "30")]
     pub older: u64,
@@ -9,14 +25,6 @@ pub struct Config {
     /// Keep at least <KEEP> generations
     #[clap(short, long, default_value = "10")]
     pub keep: usize,
-
-    /// Only list generations with their age, don't remove them
-    #[clap(long)]
-    pub list: bool,
-
-    /// Run nix garbage collection afterwards
-    #[clap(long)]
-    pub gc: bool,
 
     /// Apply to the system profile
     #[clap(short, long)]
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::fmt::Display;
+use std::io::Write;
 use std::{env, process};
 use colored::Colorize;
 
 use clap::Parser;
+use generations::Generation;
 
 mod config;
 mod gc;
@@ -39,6 +41,52 @@ fn mark(generations: &mut [generations::Generation], config: &config::Config) {
     }
 }
 
+fn ask(question: &str) -> Result<bool, String> {
+    loop {
+        print!("{} [y/n] ",question);
+        let _ = std::io::stdout().flush();
+
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input)
+            .map_err(|e| format!("Unable to ask question ({})", e))?;
+
+        match input.trim() {
+            "y" | "Y" | "yes" | "Yes" | "YES" => return Ok(true),
+            "n" | "N" | "no" | "No" | "NO" => return Ok(false),
+            _ => continue,
+        }
+    }
+}
+
+fn list_generations(generations: &[Generation], user: Option<&str>) {
+    match user {
+        None => println!("{}", format!("=> Listing system generations").green()),
+        Some(u) => println!("{}", format!("=> Listing profile generations for user {}", u).green()),
+    };
+
+    for gen in generations {
+        let marker = if gen.marked() { "would remove".red() } else { "would keep".green() };
+        let id_str = format!("[{}]", gen.number()).yellow();
+        println!("{}\t {} days old, {}", id_str, gen.age(), marker);
+    }
+}
+
+fn remove_generations(generations: &[Generation], user: Option<&str>) {
+    match &user {
+        None => println!("{}", format!("=> Removing old system generations").green()),
+        Some(user) => println!("{}", format!("=> Removing old profile generations for user {}", user).green()),
+    };
+
+    for gen in generations {
+        if gen.marked() {
+            println!("{}", format!("-> Removing generation {} ({} days old)", gen.number(), gen.age()).bright_blue());
+            resolve(gen.remove());
+        } else {
+            println!("{}", format!("-> Keeping generation {} ({} days old)", gen.number(), gen.age()).bright_black());
+        }
+    }
+}
+
 fn main() {
     let config = config::Config::parse();
 
@@ -50,32 +98,25 @@ fn main() {
     };
     mark(&mut generations, &config);
 
-    if config.list {
-        match user {
-            None => println!("{}", format!("=> Listing system generations").green()),
-            Some(u) => println!("{}", format!("=> Listing profile generations for user {}", u).green()),
-        };
-
-        for gen in generations {
-            let marker = if gen.marked() { "would be removed".red() } else { "would be kept".green() };
-            let id_str = format!("[{}]", gen.number()).yellow();
-            let age_str = format!("{} {}d", "age:".bright_blue(), gen.age());
-            println!("{}\t{}\t{}", id_str, age_str, marker);
-        }
+    if config.list || (!config.list && !config.rm && !config.gc && !config.interactive) {
+        // list generations
+        list_generations(&generations, user.as_deref());
         process::exit(0);
     }
 
-    match &user {
-        None => println!("{}", format!("=> Removing old system generations").green()),
-        Some(user) => println!("{}", format!("=> Removing old profile generations for user {}", user).green()),
-    };
-    for gen in generations {
-        if gen.marked() {
-            println!("{}", format!("-> Removing generation {} ({} days old)", gen.number(), gen.age()).bright_blue());
-            resolve(gen.remove());
-        } else {
-            println!("{}", format!("-> Keeping generation {} ({} days old)", gen.number(), gen.age()).bright_black());
+    if config.rm || config.interactive {
+        if config.interactive {
+            list_generations(&generations, user.as_deref());
+            println!();
+            let confirmation = resolve(ask("Do you want to proceed?"));
+            if !confirmation {
+                println!();
+                println!("-> Not touching anything");
+                process::exit(1);
+            }
         }
+
+        remove_generations(&generations, user.as_deref());
     }
 
     if config.gc {
