4019948b4db870aa03c4e1377f118cf98540bd79
jzbor <jzbor@posteo.net>
2025-03-24 23:35:48 +0100

    Adding basic nixos module

--- flake.nix
+++ flake.nix
@@ -23,5 +23,43 @@
       # Additional tools
       nativeBuildInputs = [];
     };
+
+    nixosModules.default = { lib, config, pkgs, ...}: let
+      cfg = config.services.nix-sweep;
+    in {
+      options.services.nix-sweep = {
+        enable = lib.mkEnableOption "Enable nix-sweep";
+
+        package = lib.mkOption {
+          type = lib.types.package;
+          default = self.packages.${pkgs.system}.default;
+          description = "nix-sweep package to use for the service";
+        };
+
+        interval = lib.mkOption {
+          type = lib.types.str;
+          default = "daily";
+          description = "How often to run nix-sweep (see systemd.time(7) for the format).";
+        };
+      };
+
+      config = lib.mkIf cfg.enable {
+        systemd.timers."nix-sweep" = {
+          wantedBy = [ "timers.target" ];
+          timerConfig = {
+            OnCalendar = cfg.interval;
+            Unit = "nix-sweep.service";
+          };
+        };
+
+        systemd.services.nix-sweep = {
+          script = "${cfg.package}/bin/nix-sweep --rm --system";
+          serviceConfig = {
+            Type = "oneshot";
+            User = "root";
+          };
+        };
+      };
+    };
   });
 }
